ce3e1cd888095163099541a405a4a11eceed28da
luchen <378979705@qq.com>
2015-07-04 01:09:38 +0800

    add comment

---
+++ src/main/java/com/morningsidevc/enums/CounterType.java
@@ -0,0 +1,9 @@
+package com.morningsidevc.enums;
+
+/**
+ * @author float.lu
+ */
+public class CounterType {
+    public static Byte FEED = Byte.parseByte("0");//动态
+    public static Byte COMMENT = Byte.parseByte("1");//评论
+}
