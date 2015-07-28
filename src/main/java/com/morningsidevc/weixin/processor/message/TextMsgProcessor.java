248ce77b730c76d5bf170822dfce9dcf27ee8002
liang.liu <liang.liu@dianping.com>
2015-07-28 13:30:33 +0800

    微信公众号开发

--- src/main/java/com/morningsidevc/weixin/processor/message/TextMsgProcessor.java
+++ src/main/java/com/morningsidevc/weixin/processor/message/TextMsgProcessor.java
@@ -1,10 +1,16 @@
 package com.morningsidevc.weixin.processor.message;
 
+import com.morningsidevc.crawler.HTMLBean;
+import com.morningsidevc.crawler.HTMLCrawlerUtils;
+import com.morningsidevc.service.FeedInfoService;
 import com.morningsidevc.weixin.bo.XmlMessageBO;
 import com.morningsidevc.weixin.processor.WeiXinBaseProcessor;
 import com.morningsidevc.weixin.replymessage.util.MsgConvertUtil;
 import com.morningsidevc.weixin.replymessage.xml.XmlText;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
+import javax.annotation.Resource;
 import java.util.Map;
 
 /**
@@ -12,12 +18,48 @@ import java.util.Map;
  */
 public class TextMsgProcessor extends WeiXinBaseProcessor {
 
+    private final static Logger logger = Logger.getLogger(TextMsgProcessor.class);
+
+    @Resource
+    private FeedInfoService feedInfoService;
+
     @Override
     public String processRequest(Map<String, String> requestMap) {
 
-        String jsContent = "欢迎进入技师模式，目前技师模式只提供与技师文本聊天功能，谢谢！";
+        String message = requestMap.get("Content");
+
+        String replyMsg = null;
+        if (StringUtils.isBlank(message)) {
+            replyMsg = "请输入你要发表的说说或推荐的网址链接，谢谢！";
+        } else {
+            if (message.startsWith("http://") || message.startsWith("https://")) {
+                HTMLBean html = HTMLCrawlerUtils.get(message);
+                if (html == null || StringUtils.isBlank(html.getUrl()) || StringUtils.isBlank(html.getPageTitle()) || StringUtils.isBlank(html.getPageAbstract())) {
+                    replyMsg = "获取推荐网址信息失败！";
+                } else {
+                    try {
+                        feedInfoService.addFeed(3, html.getUrl(), html.getPageTitle(), html.getPageAbstract(), "");
+                    } catch (Exception e) {
+                        replyMsg = "抱歉，服务器添加推荐链接异常！";
+                        logger.error("feedInfoService add link error!", e);
+                    }
+                }
+            } else {
+                try {
+                    feedInfoService.addFeed(3, message, "");
+                } catch (Exception e) {
+                    replyMsg = "抱歉，服务器添加说说异常！";
+                    logger.error("feedInfoService add message error!", e);
+                }
+            }
+        }
+
+        if (StringUtils.isBlank(replyMsg)) {
+            replyMsg = "发表成功～～";
+        }
+
         XmlText xmlText = XmlMessageBO.prepareXmlText(requestMap);
-        xmlText.setContent(jsContent);
+        xmlText.setContent(replyMsg);
         return MsgConvertUtil.parseMsg2XMLStr(xmlText);
     }
 
