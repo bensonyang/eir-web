83979909ea0b855a843b7bc527bba92d7d310937
liang.liu <liang.liu@dianping.com>
2015-08-30 18:32:13 +0800

    微信公众号功能开发

--- src/main/java/com/morningsidevc/wechart/processor/message/TextMsgProcessor.java
+++ src/main/java/com/morningsidevc/wechart/processor/message/TextMsgProcessor.java
@@ -1,7 +1,10 @@
 package com.morningsidevc.wechart.processor.message;
 
 import com.morningsidevc.wechart.bo.WeChartAddFeedBO;
+import com.morningsidevc.wechart.bo.XmlMessageBO;
 import com.morningsidevc.wechart.processor.WeChartBaseProcessor;
+import com.morningsidevc.wechart.replymessage.util.MsgConvertUtil;
+import com.morningsidevc.wechart.replymessage.xml.XmlText;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,7 +24,10 @@ public class TextMsgProcessor extends WeChartBaseProcessor {
             return WeChartAddFeedBO.processInAddFeedMode(requestMap);
         }
 
-        return "";
+        String message = "";
+        XmlText xmlText = XmlMessageBO.prepareXmlText(requestMap);
+        xmlText.setContent(message);
+        return MsgConvertUtil.parseMsg2XMLStr(xmlText);
     }
 
 }
